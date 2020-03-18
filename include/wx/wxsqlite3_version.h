4e5fc9dd1aa40abf296580a2cd2fe63637be0b0b
Hong Bin Han <36065938+kankouhin@users.noreply.github.com>
2020-03-18 10:34:51 +0900

    Add files via upload

---
+++ include/wx/wxsqlite3_version.h
@@ -0,0 +1,21 @@
+/*
+** Name:        wxsqlite3_version.h
+** Purpose:     wxSQLite3 version numbers
+** Author:      Ulrich Telle
+** Created:     2017-02-01
+** Copyright:   (c) 2017-2020 Ulrich Telle
+** License:     LGPL-3.0+ WITH WxWindows-exception-3.1
+*/
+
+/// \file wxsqlite3_version.h Version information for the wxSQLite3 library
+
+#ifndef _WXSQLITE3_VERSION_H_
+#define _WXSQLITE3_VERSION_H_
+
+#define WXSQLITE3_MAJOR_VERSION      4
+#define WXSQLITE3_MINOR_VERSION      5
+#define WXSQLITE3_RELEASE_NUMBER     1
+#define WXSQLITE3_SUBRELEASE_NUMBER  0
+#define WXSQLITE3_VERSION_STRING     "wxSQLite3 4.5.1"
+
+#endif // _WXSQLITE3_VERSION_H_
