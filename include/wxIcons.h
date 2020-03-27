b81c08c243753bbf483c2d2b3676bce8f8215bf0
Hong Bin Han <36065938+kankouhin@users.noreply.github.com>
2020-03-27 16:03:42 +0900

    Add files via upload

---
+++ include/wxIcons.h
@@ -0,0 +1,11 @@
+
+#include <icons/sample.xpm>
+#include <icons/copy.xpm>
+#include <icons/cut.xpm>
+#include <icons/help.xpm>
+#include <icons/new.xpm>
+#include <icons/open.xpm>
+#include <icons/preview.xpm>
+#include <icons/print.xpm>
+#include <icons/save.xpm>
+
