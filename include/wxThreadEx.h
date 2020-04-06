0de7465fbc1ac867aaadd807fa3706363971195b
Hong Bin Han <36065938+kankouhin@users.noreply.github.com>
2020-04-06 12:11:35 +0900

    Add files via upload

--- include/wxThreadEx.h
+++ include/wxThreadEx.h
@@ -2,36 +2,50 @@
 #ifndef __wxThreadJoinable_H__
 #define __wxThreadJoinable_H__
 
+typedef void (*pwxThreadProc)();
+
 class wxThreadJoinable : public wxThread
 {
 public:
-    wxThreadJoinable();
+//	wxThreadJoinable()
+    wxThreadJoinable(pwxThreadProc onentry, pwxThreadProc onexit = NULL);
 
     // thread execution starts here
-    virtual void *Entry() wxOVERRIDE;
+    virtual void* Entry() wxOVERRIDE;
 
     // called when the thread exits - whether it terminates normally or is
     // stopped with Delete() (but not when it is Kill()ed!)
     virtual void OnExit() wxOVERRIDE;
 
-public:
-	
-	void (*OnRun)();
+private:
+	pwxThreadProc OnEntry;
+	pwxThreadProc OnQuit;
 };
 
+/*
 inline wxThreadJoinable::wxThreadJoinable()
         : wxThread(wxTHREAD_JOINABLE)
 {
 }
+*/
+
+inline wxThreadJoinable::wxThreadJoinable(pwxThreadProc onentry, pwxThreadProc onexit)
+        : wxThread(wxTHREAD_JOINABLE)
+{
+	OnEntry = onentry;
+	OnQuit  = onexit;
+}
 
 inline void wxThreadJoinable::OnExit()
 {
+	if (OnQuit)
+		OnQuit();
 }
 
-inline wxThread::ExitCode wxThreadJoinable::Entry()
+inline void* wxThreadJoinable::Entry()
 {
-	if (OnRun)
-		OnRun();
+	if (OnEntry)
+		OnEntry();
 
     return NULL;
 }
